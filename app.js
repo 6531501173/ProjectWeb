1fe36eee67c404a38d207f1994e3284cb406212a
mevyst2003 <6531501032@lamduan.mfu.ac.th>
2024-04-19 09:00:56 +0700

    major update backend

--- app.js
+++ app.js
@@ -3,143 +3,294 @@ const path = require("path");
 const bcrypt = require("bcrypt");
 const app = express();
 var mysql = require('mysql2');
+const session = require("express-session");
 
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "library"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "library"
 });
+
+app.use(session({
+  cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  secret: "mysecretcode",
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// -----varchar to hash------
-app.get("/password/:pass", function (req, res) {
-    const password = req.params.pass;
-    const saltRounds = 10;
 
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-        if (err) {
-            return res.status(500).send("Hashing error");
-        }
-        res.send(hash);
-    });
+
+
+
+app.get("/logout", function (req, res) {
+  // Clear session variable
+  req.session.destroy(function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Cannot clear session");
+    } else {
+      res.redirect("/Home");
+    }
+  });
 });
+
+
+
+
 
 // --LOGIN PAGE----
+
+// -----varchar to hash------
+// app.get("/password/:pass", function (req, res) {
+//     const password = req.params.pass;
+//     const saltRounds = 10;
+
+//     bcrypt.hash(password, saltRounds, function (err, hash) {
+//         if (err) {
+//             return res.status(500).send("Hashing error");
+//         }
+//         res.send(hash);
+//     });
+// });
+
+
 app.get("/login", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/index/login.html"));
+  res.sendFile(path.join(__dirname, "views/index/login.html"));
 });
 app.post("/login", (req, res) => {
-    const { username, password } = req.body;
-  
-    const sql = "SELECT user_id, password, role FROM user_info WHERE username = ?";
-    con.query(sql, [username], (err, results) => {
+  const { username, password } = req.body;
+
+  const sql = "SELECT user_id, password, role FROM user_info WHERE username = ?";
+  con.query(sql, [username], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database server error");
+    }
+    if (results.length !== 1) {
+      return res.status(401).send("Wrong username");
+    }
+    bcrypt.compare(password, results[0].password, (err, same) => {
       if (err) {
-        console.error(err);
-        return res.status(500).send("Database server error");
+        return res.status(500).send("Authentication server error");
       }
-      if (results.length !== 1) {
-        return res.status(401).send("Wrong username");
+      if (same) {
+        req.session.userID = results[0].user_id;
+        req.session.username = username;
+        req.session.role = results[0].role; // Set the user's role in the session
+        const redirectTo = "/assetlist";
+        return res.status(200).json({ redirectTo });
+      } else {
+        return res.status(401).send("Wrong password");
       }
-      bcrypt.compare(password, results[0].password, (err, same) => {
-        if (err) {
-          return res.status(500).send("Authentication server error");
-        }
-        if (same) {
-          // Redirect based on user's       role ไปตามหน้าของแต่ละrole
-          let redirectTo;
-          switch (results[0].role) {
-            case "admin":
-              redirectTo = "/staff/dashboard";
-              break;
-            case "lender":
-              redirectTo = "/lender/booklist";
-              break;
-            case "borrower":
-              redirectTo = "/borrower/booklist";
-              break;
-            default:
-              redirectTo = "/";
-              break;
-          }
-          return res.status(200).json({ redirectTo });
-        } else {
-          return res.status(401).send("Wrong password");
-        }
-      });
     });
   });
+});
+
+
 
 
 
 // ------REGISTER PAGE------
 
 app.get("/register", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/index/register.html"));
+  res.sendFile(path.join(__dirname, "views/index/register.html"));
 });
 
 app.post("/register", function (req, res) {
-    const name = req.body.name;
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+  const name = req.body.name;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    bcrypt.hash(password, 10, function (err, hash) {
-        if (err) {
-            return res.status(500).send("Hashing error");
-        }
-        var sql = `INSERT INTO user_info (name, username, email, password) VALUES (?, ?, ?, ?)`; // mysql ใส่ตารางทั้งหมดของดาต้า user
-        con.query(sql, [name, username, email, hash], function (err, result) {// ส่งคำสั่งไปใน mysql ดาต้าของเรา
-            if (err) {
-                console.error(err);
-                return res.status(500).send("Database server error");
-            }
-            // If registration is successful, send a success response
-            return res.status(200).send("Registration successful");
-        });
+  bcrypt.hash(password, 10, function (err, hash) {
+    if (err) {
+      return res.status(500).send("Hashing error");
+    }
+    var sql = `INSERT INTO user_info (name, username, email, password) VALUES (?, ?, ?, ?)`; // mysql ใส่ตารางทั้งหมดของดาต้า user
+    con.query(sql, [name, username, email, hash], function (err, result) {// ส่งคำสั่งไปใน mysql ดาต้าของเรา
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Database server error");
+      }
+      // If registration is successful, send a success response
+      return res.status(200).send("Registration successful");
     });
+  });
 });
 
 
-// -------STAFF----------
-app.get("/staff/dashboard", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/staff/Dashboard.html"));
-});
-app.get("/staff/booklist", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/staff/Assetlist.html"));
-});
-app.get("/staff/create", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/staff/Create.html"));
-});
-app.get("/staff/edit", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/staff/Edit.html"));
-});
-app.get("/staff/history", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/staff/History.html"));
-});
-app.get("/staff/returnasset", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/staff/Returnasset.html"));
+
+
+
+
+
+//  ------Assetlist-------
+app.get("/assetlist", function (req, res) {
+  // Check if the user is logged in
+  if (!req.session.userID) {
+    return res.status(200).redirect("/Home");
+  }
+  let sql;
+  // Fetch user role from the session
+  let filePath;
+  const userRole = req.session.role;
+  switch (userRole) {
+    case "admin":
+      filePath = path.join(__dirname, "views", "staff", "Assetlist.html");
+      break;
+    case "lender":
+      filePath = path.join(__dirname, "views", "lender", "Assetlist.html");
+      break;
+    case "borrower":
+      filePath = path.join(__dirname, "views", "borrower", "Assetlist.html");
+      break;
+    default:
+      return res.status(404).send("Page not found");
+  }
+  res.sendFile(filePath);
 });
 
-// ------Lender----------
-app.get("/lender/booklist", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/Lender/Assetlist.html"));
+app.get("/getbooklist", function (req, res) {
+  const userRole = req.session.role;
+  if (userRole == 'admin') {
+    sql = "SELECT assetlist.*, user_info.name AS lender_name FROM assetlist INNER JOIN user_info ON assetlist.lender_id = user_info.user_id";
+  } else {
+    sql = "SELECT * FROM assetlist WHERE status = 'available'";
+  }
+  con.query(sql, function (err, results) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database server error");
+    }
+    res.json(results);
+  });
 });
 
-// ------Borrower--------
-app.get("/borrower/booklist", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/borrower/Assetlist.html"));
+// ------EDIT ASSETLIST(ADMIN)------
+app.get("/assetlist/edit", function (req, res) {
+  if (!req.session.userID) {
+    return res.status(200).redirect("/Home");
+  }
+  res.sendFile(path.join(__dirname, "/views/staff/Edit.html"));
 });
+
+
+
+
+
+
+// ------Borrow request-----
+app.get("/findbook", function (req, res) {
+  const bookId = req.query.bookid; // Retrieve the bookid from the query parameters
+  const sql = "SELECT * FROM assetlist WHERE book_id = ?";
+  con.query(sql, [bookId], function (err, results) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database server error");
+    }
+    if (results.length !== 1) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    res.json(results[0]); // Return the book information
+  });
+});
+
+app.get("/requestborrow/bookid=:bookId", function (req, res) {
+  if (!req.session.userID) {
+    return res.status(200).redirect("/Home");
+  }
+  res.sendFile(path.join(__dirname, "/views/borrower/Requestborrow.html"));
+});
+
+app.post("/requestborrow", function (req, res) {
+  const bookId = req.body.bookId;
+  res.status(200).json({ redirectTo: `/requestborrow/bookid=${bookId}`, bookId: bookId });
+});
+
+
+
+
+
+// ------Request Status-----
+app.get("/requestStatus", function (req, res) {
+  if (!req.session.userID) {
+    return res.status(200).redirect("/Home");
+  }
+  res.sendFile(path.join(__dirname, "/views/borrower/Requeststatus.html"));
+});
+app.get("/getStatus", function (req, res) {
+  const userId = req.session.userID;
+  const sql = "SELECT r.*, a.image FROM request r JOIN assetlist a ON r.book_id = a.book_id WHERE r.borrower_id = ?";
+  con.query(sql, [userId], function (err, results) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database server error");
+    }
+    res.json(results);
+  });
+});
+
+app.put("/cancelRequest", function (req, res) {
+  const requestId = req.query.requestId;
+  const sql = "UPDATE request SET request_status = 'cancelled' WHERE request_id = ?";
+  con.query(sql, [requestId], function (err, result) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database server error");
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).send("Request not found");
+    }
+    return res.status(200).send("Request cancelled successfully");
+  });
+});
+
+
+app.post("/requestStatus", function (req, res) {
+  const bookId = req.body.bookid; // Retrieve bookId from the request body
+  const sql = "SELECT * FROM assetlist WHERE book_id = ?";
+  const borrower_id = req.session.userID;
+  const borrow_date = req.body.borrowerDate;
+  const return_date = req.body.returnDate;
+  con.query(sql, [bookId], function (err, results) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database server error");
+    }
+    if (results.length !== 1) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    const sql_ins = "INSERT INTO request (book_id, lender_id, borrower_id, borrow_date, return_date, title) VALUES (?, ?, ?, ?, ?, ?)";
+    const params = [results[0].book_id, results[0].lender_id, borrower_id, borrow_date, return_date, results[0].title];
+    con.query(sql_ins, params, function (err, req) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Database server error");
+      }
+      res.status(200).send("INSERT COMPLETED!!!");
+    });
+  });
+});
+
+
+
 // ------Root-----------
 app.get("/Home", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/index/Home.html"));
+  if (req.session.username) {
+    res.redirect("/Assetlist");
+  } else {
+    res.sendFile(path.join(__dirname, "views", "index", "Home.html"));
+  }
 });
+
 
 const port = 3000;
 app.listen(port, function () {
-    console.log("Server is ready at " + port);
+  console.log("Server is ready at " + port);
 });
